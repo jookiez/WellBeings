0fbcebc8083857bf11061d16081ee6ede406eb8c
Matt Wagner <wagnerm@alumni.uoguelph.ca>
2020-09-01 20:43:06 -0400

    finishing up content page.

--- MattWagnerWellBeings/ClientApp/src/app/contact/contact.component.ts
+++ MattWagnerWellBeings/ClientApp/src/app/contact/contact.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Local } from 'protractor/built/driverProviders';
 
 @Component({
   selector: 'app-contact',
-  templateUrl: './contact.component.html'
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+  public submitted: boolean;
+  private readonly _tokenKey = 'form_submitted';
   constructor() {
+    this.submitted = false;
+  }
+
+  public ngOnInit() {
+    if (localStorage.getItem(this._tokenKey) === "true") {
+      this.submitted = true;
+    }
+  }
+
+  public submitForm() {
+    localStorage.setItem(this._tokenKey, "true");
+    this.submitted = true;
   }
 }
